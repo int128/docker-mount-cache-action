7530e88b85fc452579defab78b3d05e0a35c859d
Hidetake Iwata <int128@gmail.com>
2023-12-22 22:20:39 +0900

    Test

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import * as core from '@actions/core'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  await run({
-    name: core.getInput('name', { required: true }),
-  })
+  await run({})
 }
 
 main().catch((e: Error) => {
--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,26 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as os from 'os'
+import * as fs from 'fs/promises'
 
-type Inputs = {
-  name: string
-}
+type Inputs = object
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+  core.info(`GITHUB_ACTION_PATH=${process.env.GITHUB_ACTION_PATH}`)
+
+  inputs
+
+  const tempDir = await fs.mkdtemp(process.env.RUNNER_TEMP || os.tmpdir())
+
+  await fs.writeFile(
+    `${tempDir}/Dockerfile`,
+    `
+FROM busybox:1
+RUN --mount=type=cache,target="$(read_action_input cache-target)" \
+    --mount=type=bind,source=.,target=/var/dance-cache \
+    cp -p -R /var/dance-cache/. "$(read_action_input cache-target)" || true
+`,
+  )
+
+  await exec.exec('docker', ['buildx', 'build', '-f'])
 }
