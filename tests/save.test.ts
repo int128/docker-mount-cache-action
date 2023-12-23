e6582edfee1b12bce1fac0a683e9cca98b5d4ca0
Hidetake Iwata <int128@gmail.com>
2023-12-23 17:30:48 +0900

    Fix

--- tests/save.test.ts
+++ tests/save.test.ts
@@ -1,5 +1,5 @@
 import { run } from '../src/save/run'
 
 test('run successfully', async () => {
-  await expect(run({ key: 'foo', path: 'bar' })).resolves.toBeUndefined()
+  await expect(run({ tags: 'foo', path: 'bar' })).resolves.toBeUndefined()
 })
