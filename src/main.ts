2c4e6b86658c6ee5a703e2400f3f635e8db6d1cd
Hidetake Iwata <int128@gmail.com>
2023-12-25 07:13:18 +0900

    Split action to save and restore (#4)

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import * as core from '@actions/core'
 import * as fs from 'fs/promises'
 import * as os from 'os'
 import * as path from 'path'
-import { restoreCache } from './run'
+import { run } from './run'
 
 const main = async (): Promise<void> => {
-  await restoreCache({
+  await run({
     path: core.getInput('path', { required: true }),
-    fromTags: core.getMultilineInput('from-tags', { required: true }),
-    toTags: core.getMultilineInput('to-tags', { required: true }),
+    tags: core.getMultilineInput('tags', { required: true }),
     contextDir: await fs.mkdtemp(path.join(process.env.RUNNER_TEMP || os.tmpdir(), 'context-')),
+    internalMode: core.getInput('internal-mode', { required: true }),
   })
 }
 
